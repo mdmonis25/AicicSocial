21c9ab7af08c4edcf24f869fbbd1aafad2e2b148
Vishal Ramteke <vishalramteke99@gmail.com>
2024-02-23 14:55:49 +0530

    bootom tab fixed

--- src/navigations/BottomTabNavigator.js
+++ src/navigations/BottomTabNavigator.js
@@ -3,7 +3,7 @@ import {colors, sizes} from '../constants/theme';
 
 import Favorite from '../screens/FavoriteScreen';
 import HomePage from '../screens/SearchScreen';
-import Icon from '../components/shared/Icon';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Profile from '../screens/profile/ProfileScreen';
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -45,23 +45,39 @@ const Root = () => {
   return (
   
       <Tab.Navigator initialRouteName='HomePage'  activeColor='#7F27FF' inactiveColor='gray' barStyle={{backgroundColor: '#ffffff'}}>
-        <Tab.Screen name = "HomePage" component={HomePage} />
-        <Tab.Screen name = "Profile" component={Profile} />
+        <Tab.Screen 
+        name = "HomePage"
+         component={HomePage}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          )
+        }} />
+        <Tab.Screen
+         name = "Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            )
+          }} />
       </Tab.Navigator>
   
   );
 };
 
 const styles = StyleSheet.create({
-  indicator: {
-    position: 'absolute',
-    width: 10,
-    height: 2,
-    left: sizes.width / tabs.length / 2 - 5,
-    bottom: 30,
-    backgroundColor: colors.primary,
-    zIndex: 100,
-  },
+  // indicator: {
+  //   position: 'absolute',
+  //   width: 10,
+  //   height: 2,
+  //   left: sizes.width / tabs.length / 2 - 5,
+  //   bottom: 30,
+  //   backgroundColor: colors.primary,
+  //   zIndex: 100,
+  // },
 });
 
 export default Root;
