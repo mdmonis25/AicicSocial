b530311bbaf0f9aa0f830911fcc6481811a08c22
mdmonis <mdmonis128@gmail.com>
2024-03-06 17:45:01 +0530

    Search Screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,103 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, FlatList, StyleSheet, Image,TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; // Assuming you're using Ionicons for the back icon
+
+const SearchScreen = () => {
+  const navigaton = useNavigation();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [dummyData, setDummyData] = useState([
+    { id: '1', name: 'John Doe', image: require('../../assets/images/users/32.jpeg') },
+    { id: '2', name: 'Jane Smith', image: require('../../assets/images/users/32.jpeg') },
+    { id: '3', name: 'Alice Johnson', image: require('../../assets/images/users/35.jpeg') },
+    // Add more dummy data as needed
+  ]);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.card}>
+      <Image source={item.image} style={styles.userImage} />
+      <Text style={styles.userName}>{item.name}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.backButton} onPress={()=>{navigaton.goBack()}}>
+          <Icon name="arrow-back" size={24} color="#333" />
+        </TouchableOpacity>
+        <Text style={styles.title}>Search Users</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Search..."
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+      <FlatList
+        data={dummyData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    backgroundColor: '#f8f8f8',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  backButton: {
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    flex: 1,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    color:'#000'
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginBottom: 10,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default SearchScreen;
