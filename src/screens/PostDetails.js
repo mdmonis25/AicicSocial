7027155ea80b7f362d38cc8c47cf34018c6d2d8e
mdmonis <mdmonis128@gmail.com>
2024-03-15 14:49:59 +0530

    PostDetails Screen Added

---
+++ src/screens/PostDetails.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PostDetails = () => {
+  return (
+    <View>
+      <Text>PostDetails</Text>
+    </View>
+  )
+}
+
+export default PostDetails
